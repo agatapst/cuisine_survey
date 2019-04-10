59b12ddf2cb06b81fd0e67ab08a07410d63c491b
Agata Stawarz <agata.stawarz94@gmail.com>
2019-04-10 10:29:52 +0200

    Add initial project structure

---
+++ app.js
@@ -0,0 +1,21 @@
+new Vue({
+    el: '#vue-app',
+    data: {
+        types: [
+            {name: 'italian', imageUrl: 'https://images.unsplash.com/photo-1528137871618-79d2761e3fd5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80.jpg'},
+            // {name: 'french', imageUrl: 'https://images.unsplash.com/photo-1546020037-0a343efa835a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80.jpg'},
+            {name: 'spanish', imageUrl: 'https://images.unsplash.com/photo-1512058466835-da4d54fb0ee8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=960&q=80.jpg'},
+            {name: 'israeli', imageUrl: 'https://images.unsplash.com/photo-1547058881-aa0edd92aab3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80.jpg'},
+            {name: 'mexican', imageUrl: 'https://images.unsplash.com/photo-1548228586-171fb0887ac0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=970&q=80'},
+            {name: 'thai', imageUrl: 'https://images.unsplash.com/photo-1455619452474-d2be8b1e70cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80.jpg'},
+            {name: 'chinese', imageUrl: 'https://images.unsplash.com/photo-1550303659-a777c03a27bd?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80.jpg'},
+       ],
+    },
+
+    methods: {
+
+    },
+    computed: {
+       
+    }
+});
