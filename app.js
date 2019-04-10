d1614bf83cb6c4f1a92f0c23e1b7c521560ff41d
Agata Stawarz <agata.stawarz94@gmail.com>
2019-04-10 16:22:27 +0200

    Add cuisine card as a component

--- app.js
+++ app.js
@@ -1,36 +1,37 @@
-Vue.component('vote-button', {
-    template: '#vote-button',
-    props: {
-      voteForType: Function
-    },
-    data: function () {
-      return {
-        count: 0
-      }
+Vue.component('cuisine-card', {
+  template: '#cuisine-card',
+  props: {
+    cuisineType: Object,
+  }, 
+  
+  methods: {
+    addPoint: function(cuisineType) {
+      console.log("point added: " + cuisineType.name)
+      cuisineType.votes++;
     }
-}),
+  }
+})
 
 new Vue({
-    el: '#vue-app',
-    data: {
-        types: [
-            {name: 'italian', desc: 'pizza, spaghetti', votes: '0', imageUrl: 'https://images.unsplash.com/photo-1528137871618-79d2761e3fd5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80.jpg'},
-            {name: 'spanish', desc: 'paella, jamon', votes: '0', imageUrl: 'https://images.unsplash.com/photo-1512058466835-da4d54fb0ee8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=960&q=80.jpg'},
-            {name: 'israeli', desc: 'falafel, hummus', votes: '0', imageUrl: 'https://images.unsplash.com/photo-1547058881-aa0edd92aab3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80.jpg'},
-            {name: 'mexican', desc: 'epanadas, guacamole', votes: '0', imageUrl: 'https://images.unsplash.com/photo-1548228586-171fb0887ac0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=970&q=80'},
-            {name: 'thai', desc: 'pad thai, red curry', votes: '0', imageUrl: 'https://images.unsplash.com/photo-1455619452474-d2be8b1e70cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80.jpg'},
-            {name: 'chinese', desc: 'bao ze, noodle', votes: '0', imageUrl: 'https://images.unsplash.com/photo-1550303659-a777c03a27bd?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80.jpg'},
-       ],
-    },
-
-    methods: {
-        addPoint: function(type) {
-          console.log("point added: " + type.name)
-          type.votes++;
-        }
-
-    },
-    computed: {
-       
+  el: '#vue-app',
+  data: {
+    types: [
+      {name: 'italian', desc: 'pizza, spaghetti', votes: 0, imageUrl: 'https://images.unsplash.com/photo-1528137871618-79d2761e3fd5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80.jpg'},
+      {name: 'spanish', desc: 'paella, jamon', votes: 0, imageUrl: 'https://images.unsplash.com/photo-1512058466835-da4d54fb0ee8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=960&q=80.jpg'},
+      {name: 'israeli', desc: 'falafel, hummus', votes: 0, imageUrl: 'https://images.unsplash.com/photo-1547058881-aa0edd92aab3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80.jpg'},
+      {name: 'mexican', desc: 'epanadas, guacamole', votes: 0, imageUrl: 'https://images.unsplash.com/photo-1548228586-171fb0887ac0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=970&q=80'},
+      {name: 'thai', desc: 'pad thai, red curry', votes: 0, imageUrl: 'https://images.unsplash.com/photo-1455619452474-d2be8b1e70cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80.jpg'},
+      {name: 'chinese', desc: 'bao ze, noodle', votes: 0, imageUrl: 'https://images.unsplash.com/photo-1550303659-a777c03a27bd?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80.jpg'},
+    ],
+  },
+  
+  computed: {
+    sortedTypes: function(){
+      return this.types.concat().sort((type1, type2) => {
+        if(type1.votes < type2.votes) { return 1; }
+        if(type1.votes > type2.votes) { return -1; }
+        return 0
+      });
     }
-});
+  }
+})
